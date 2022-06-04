1ae5560ab49a6a9ac1971f1ce74c3781be39672c
Jacob Hampton <jhampton792@gmail.com>
2022-06-04 10:52:12 -0500

    updated factorials recursion file

--- tests/recursion/factorials.test.js
+++ tests/recursion/factorials.test.js
@@ -14,11 +14,6 @@
 function factorial(n) { 
   
   if (n > 1) return n * factorial(n - 1);
-  // 5 * factorial(4)           // 5 * 24 = 120
-  // // 4 * factorial(3)        // 4 * 6 = 24
-  // // // 3 * factorial(2)     // 3 * 2 = 6
-  // // // // 2 * factorial(1)  // 2 * 1 = 2
-  // // // // // 1              // 1 = 1
   return 1;
 
   // not recursively but better?
