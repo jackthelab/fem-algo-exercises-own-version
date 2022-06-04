d0a91feab32b8e5d0876359cbef45e6c0031d856
Jacob Hampton <jhampton792@gmail.com>
2022-06-04 14:40:09 -0500

    added quick sort code

--- tests/sorts/quick-sort.test.js
+++ tests/sorts/quick-sort.test.js
@@ -1,8 +1,4 @@
 /*
-  Quick Sort!
-  
-  Name your function quickSort.
-  
   Quick sort should grab a pivot from the end and then separate the list (not including the pivot)
   into two lists, smaller than the pivot and larger than the pivot. Call quickSort on both of those
   lists independently. Once those two lists come back sorted, concatenate the "left" (or smaller numbers)
@@ -11,10 +7,31 @@
 */
 
 function quickSort(nums) {
-  // code goes here
+  // base case --> if nums.length < 2 already sorted, return as is
+  if (nums.length < 2) return nums;
+
+  // find the pivot -- traditional method here
+  // make sure to remove from array too or else will break call stack -- think it's an infinite loop?
+  const pivot = nums.pop();
+
+  // arrays for smaller & larger items
+  const less = [];
+  const more = [];
+
+  // sort each iterable value into less or more array
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] <= pivot) {
+      less.push(nums[i]);
+    } else {
+      more.push(nums[i]);
+    }
+  }
+
+  // combine the quick sort of less and quick sort of more around pivot
+  return quickSort(less).concat(pivot, quickSort(more));
 }
 
-describe.skip('quick sort own solutions tests', () => {
+describe('quick sort own solutions tests', () => {
 
   it("quickSort", function () {
     const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
